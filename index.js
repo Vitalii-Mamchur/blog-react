b107df75279e22c9be5a9b8b3a9f3a2133023ae9
Vitalii-Mamchur <vetalpatsi@gmail.com>
2022-12-27 22:52:05 +0200

    add register & login

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.set('strictQuery', false);
 
 mongoose
   .connect(
-    'mongodb+srv://Vitalii:dfghjc243@cluster0.6376m08.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://Vitalii:dfghjc243@cluster0.6376m08.mongodb.net/blog?retryWrites=true&w=majority'
   )
   .then(() => {
     console.log('DB Ok');
@@ -24,26 +24,87 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found!',
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(404).json({
+        message: 'Email or password is invalid!',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed to login!',
+    });
   }
+});
 
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-  const user = await doc.save();
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
 
-  res.json(user);
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed to register',
+    });
+  }
 });
 
 app.listen(4444, (err) => {
