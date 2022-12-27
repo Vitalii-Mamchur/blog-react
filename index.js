8ce4c54de883e096eb7d7c41c7840f34c4840c7b
Vitaii Mamchur <vetalpatsi@gmail.com>
2022-12-27 19:42:40 +0200

    add validations

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
+
+import registerValidation from './validations/auth.js';
+import UserModel from './models/User.js';
 
 mongoose.set('strictQuery', false);
 
@@ -19,7 +24,27 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', (req, res) => {});
+app.post('/auth/register', registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+
+  const user = await doc.save();
+
+  res.json(user);
+});
 
 app.listen(4444, (err) => {
   if (err) {
