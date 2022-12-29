555b4d5dd87162d3c3bf0314e7b09be268731bd7
Vitalii-Mamchur <vetalpatsi@gmail.com>
2022-12-29 23:53:39 +0200

    add upload functional

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -13,6 +13,45 @@ export const getAll = async (req, res) => {
   }
 };
 
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (error, doc) => {
+        if (error) {
+          console.error(error);
+          return res.status(500).json({
+            message: 'Failed to retrieve post!',
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Post not found!',
+          });
+        }
+
+        res.json(doc);
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Failed to retrieve post!',
+    });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
@@ -30,6 +69,65 @@ export const create = async (req, res) => {
     console.log(error);
     res.status(500).json({
       message: 'Failed to create post!',
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndRemove(
+      {
+        _id: postId,
+      },
+      (error, doc) => {
+        if (error) {
+          console.error(error);
+          return res.status(500).json({
+            message: 'Failed to remove post!',
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Post not found!',
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Failed to retrieve post!',
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.findOneAndUpdate(
+      { _id: postId },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      }
+    );
+
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed to update post!',
     });
   }
 };
