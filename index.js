ebfc22ecc4fed1db79d2b39070bc6a44d72a8d49
Vitalii-Mamchur <vetalpatsi@gmail.com>
2023-01-02 16:42:05 +0200

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import {
   registerValidation,
@@ -39,6 +40,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post(
@@ -61,7 +63,10 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   });
 });
 
+app.get('/tags', PostController.getLastTags);
+
 app.get('/posts', PostController.getAll);
+app.get('/post/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 app.post(
   '/posts',
